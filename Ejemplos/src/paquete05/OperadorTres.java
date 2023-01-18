ce1f7f98e9b1bb96a2ed3b1e37f064ad3206b59f
Carlos Mejia <cdmejia4@utpl.edu.ec>
2023-01-18 10:21:02 -0500

    subida clase 2

---
+++ Ejemplos/src/paquete05/OperadorTres.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author SALA I
+ */
+public class OperadorTres {
+    
+
+
+    public static int ObtenerMultiplicacion(int a, int b) {
+        
+        int multiplicacion = a * b;
+        
+        
+        return multiplicacion;
+        
+    }
+}
